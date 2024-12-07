4fa0a36181a3cee388c15afad3842770abe5e65c
Aditya Mewar <adityamewar061@gmail.com>
2024-12-07 11:25:29 +0530

    3D_Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works,  } from "./components";
+
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/3D_Portfolio">
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -17,7 +18,7 @@ const App = () => {
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
-          <StarsCanvas />
+   
         </div>
       </div>
     </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/3D_Portfolio/', // Replace with your repository name (e.g., 'my-portfolio')
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Optional, for the default build output directory
+  },
 })
