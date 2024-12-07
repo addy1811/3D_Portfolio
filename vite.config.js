9d710683efa1fcb9c95f7aa34a9f010a9f30ce00
Aditya Mewar <124774558+addy1811@users.noreply.github.com>
2024-12-07 10:31:24 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/<3D_Portfolio>/', // Replace with your repository name (e.g., 'my-portfolio')
+  base: '/3D_Portfolio/', // Replace with your repository name (e.g., 'my-portfolio')
   plugins: [react()],
   build: {
     outDir: 'dist', // Optional, for the default build output directory
